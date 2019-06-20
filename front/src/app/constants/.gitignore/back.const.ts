603bddbc92542b2ac3b18df26927ba39fc5433c6
Franck <patouillardfranck3@gmail.com>
2019-06-20 13:53:33 +0200

    cd

---
+++ front/src/app/constants/.gitignore/back.const.ts
@@ -0,0 +1,9 @@
+export const BACK = {
+  url_get: 'http://localhost:8000',
+  url_login: 'http://localhost:8000/login',
+  urlAPI: 'http://localhost:8000/api'
+};
+
+export const ROUTES = {
+  urlUser: 'users'
+};
